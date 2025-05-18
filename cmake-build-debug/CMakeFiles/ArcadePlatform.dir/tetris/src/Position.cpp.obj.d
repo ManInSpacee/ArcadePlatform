4fa99c62545c342b89f7b27042d11c7dd8bd98b4
ManInSpace <114342026+ManInSpacee@users.noreply.github.com>
2025-05-18 12:51:48 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/ArcadePlatform.dir/tetris/src/Position.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/ArcadePlatform.dir/tetris/src/Position.cpp.obj: \
- C:\Users\ManInSpace\CLionProjects\ArcadePlatform\tetris\src\Position.cpp \
- C:\Users\ManInSpace\CLionProjects\ArcadePlatform\tetris\src\Position.h
